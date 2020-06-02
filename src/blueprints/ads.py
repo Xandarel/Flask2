7eb81b827b09da28cd0f7c885fbc818a23e409db
Xandarel <aobordug@gmail.com>
2020-06-02 18:56:09 +0500

    Реализовано редактирование объявления

--- src/blueprints/ads.py
+++ src/blueprints/ads.py
@@ -187,6 +187,159 @@ class AdView(MethodView):
             else:
                 return jsonify(ad), 200
 
+    def patch(self, ad_id):
+        if session['id']:
+            with db.connection as con:
+                cur = con.execute("""
+                SELECT id
+                FROM seller
+                WHERE account_id = ?
+                """,
+                (session['id'],)
+                )
+                is_seller = cur.fetchone()
+            seller_id = dict(is_seller)
+            if not bool(seller_id['id']):
+                return '', 403
+        else:
+            return '', 401
+        with db.connection as con:
+            cur = con.execute("""
+            SELECT id
+            FROM ad
+            WHERE ad.id=? AND seller_id=?
+            """,
+            (ad_id, seller_id)
+            )
+            ad = cur.fetchone()
+        if not bool(ad):
+            return '', 412
+
+        request_json = request.json
+        title = request_json.get('title')
+        tags = request_json.get('tags')
+        car = request_json.get('car')
+
+        make = car.get('make')
+        model = car.get('model')
+        colors = car.get('colors')
+        mileage = car.get('mileage')
+        num_owners = car.get('num_owners')
+        reg_number = car.get('reg_number')
+        images = car.get('images')
+        if title:
+            with db.connection as con:
+                con.execute(""" 
+                UPDATE ad
+                SET title = ?
+                WHERE id = ?
+                """,
+                (title, ad_id)
+                )
+                con.commit()
+
+        if tags:
+            title_id = list()
+            for t in title:
+                with db.connection as con:
+                    cur = con.execute("""
+                    SELECT id 
+                    FROM tag
+                    WHERE name = ?
+                    """,
+                    (t,)
+                    )
+                    title_id.append(dict(cur.fetchone())['id'])
+
+            with db.connection as con:
+                con.execute("""
+                    DELETE FROM adtags
+                    WHERE ad_id = ?
+                """,
+                (ad_id,)
+                )
+                con.commit()
+
+            for t_id in title_id:
+                with db.connection as con:
+                    con.execute(""" 
+                        INSERT INTO adtag (tag_id, ad_id)
+                        VALUES (?, ?)
+                        """,
+                    (t_id, ad_id)
+                    )
+                    con.commit()
+
+        if car:
+            update_car = 'UPDATE car '
+            set = 'SET '
+            where = 'WHERE ad.id = ? AND ad.car_id = car.id'
+            values = list()
+            values.append(ad_id)
+            if make:
+                set += 'make = ?,'
+                values.append(make)
+            if model:
+                set += 'model = ?,'
+                values.append(model)
+            if mileage:
+                set += 'mileage = ?,'
+                values.append(mileage)
+            if num_owners:
+                set += 'num_owners = ?,'
+                values.append(num_owners)
+            if reg_number:
+                set += 'reg_number = ?,'
+                values.append(reg_number)
+            set = set[:-1]
+            with db.connection as con:
+                con.execute(set + where, values)
+
+        with db.connection as con:
+                    cur = con.execute(""" 
+                        SELECT id
+                        FROM car
+                        WHERE ad.id = ? AND ad.car_id = car.id
+                    """,
+                    (ad_id)
+                    )
+                    car_id = dict(cur.fetchone())['id']
+
+        if colors:
+            for col in colors:
+                try:
+                  with db.connection as con:
+                        con.execute(""" 
+                            INSERT INTO carcolor (color_id, car_id)
+                            VALUES (?, ?)
+                        """,
+                        (col, car_id)
+                        )
+                        con.commit()
+                except sqlite3.IntegrityError:
+                    pass
+
+        if images:
+            try:
+                for im in images:
+                    im_title = im.get('title')
+                    url = im.get('url')
+                    with db.connection as con:
+                        con.execute(""" 
+                            INSERT INTO image (title, url, car_id)
+                            VALUES (?, ?, ?)
+                        """,
+                        (im_title,url, car_id)
+                        )
+                        con.commit()
+            except sqlite3.IntegrityError:
+                pass
+
+
+
+
+
+
     def delete(self, ads_id):
         with db.connection as con:
             cur = con.execute(f"""
